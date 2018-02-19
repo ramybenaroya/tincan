a434ca717758a8f0fb3d88e844345c7c51e3a702
Ramy Ben Aroya <ramybenaroya@gmail.com>
2018-02-19 04:22:28 +0200

    initial commit

---
+++ src/tincan.js
@@ -0,0 +1,110 @@
+var v = '__VERSION__';
+
+function Tincan(otherWindow, otherOrigin) {
+	this._otherWindow = otherWindow;
+	this._otherOrigin = otherOrigin;
+	this._uid = Math.random();
+	this._counter = 0;
+	this._answer = this._onMessage.bind(this);
+	this._sessions = {};
+	window.addEventListener('message', this._answer);
+}
+
+Tincan.prototype._onMessage = function(event) {
+	var message = event.data;
+	var result;
+	var error;
+	var f;
+	var doesOriginMatch = this._doesOriginMatch(event.origin);
+	var didFinish = false;
+	var that = this;
+	if (!doesOriginMatch || !message || !message.id || message.v !== v) {
+		return;
+	}
+	if (message.f) {
+		f = new Function(message.f.replace('function', 'return function'))();
+		try {
+			if (f.length > 0) {
+				f.call(window, done, fail);
+			} else {
+				result = f.call(window);
+				done(result);
+			}
+		} catch (e) {
+			error = {
+				message: e.message,
+				stack: e.stack.toString()
+			};
+			fail(error);
+		}
+	} else {
+		if (this._sessions[message.id]) {
+			if (message.hasOwnProperty('result') && this._sessions[message.id].success) {
+				this._sessions[message.id].success(message.result);
+			} else if (message.hasOwnProperty('error') && this._sessions[message.id].fail) {
+				error = new Error(message.error.message);
+				error.stack = message.error.stack;
+				this._sessions[message.id].fail(message.error);
+			}
+			this._sessions[message.id] = null;
+			delete this._sessions[message.id];
+		}
+	}
+
+	function done(result, error) {
+		if (didFinish) {
+			return;
+		}
+		didFinish = true;
+		var messageBack = {
+			id: message.id,
+			v: v
+		};
+		if (error) {
+			messageBack.error = error;
+		} else {
+			messageBack.result = result;
+		}
+		that._otherWindow.postMessage(messageBack, that._otherOrigin);
+	}
+
+	function fail(error) {
+		return done(
+			null,
+			error
+				? {
+						message: error.message,
+						stack: error.stack
+					}
+				: null
+		);
+	}
+};
+
+Tincan.prototype.talk = function(f, success, fail) {
+	var id = ++this._counter + this._uid;
+	var message = {
+		id: id,
+		v: v,
+		f: f.toString()
+	};
+	this._sessions[id] = {
+		success: success,
+		fail: fail
+	};
+	this._otherWindow.postMessage(message, this._otherOrigin);
+};
+
+Tincan.prototype._doesOriginMatch = function(origin) {
+	return (
+		origin === this._otherOrigin ||
+		(this._otherOrigin === '*' && (window.location.origin === origin || origin === 'null'))
+	);
+};
+
+Tincan.prototype.destroy = function() {
+	window.removeEventListener('message', this._answer);
+	this._otherWindow = this._otherOrigin = this._uid = this._counter = this._answer = this._sessions = null;
+};
+
+module.exports = Tincan;
---
+++ test/parent-child.test.js
@@ -0,0 +1,85 @@
+describe('Parent Child Test', function() {
+	let page;
+	this.timeout(1000000);
+
+	before(async function() {
+		page = await browser.newPage();
+		await page.goto(`file://${__dirname}/pages/parent.html`);
+	});
+
+	after(async function() {
+		await page.close();
+	});
+
+	it('Get parent data', async function() {
+		const frame = await page.frames().find(f => f.name() === 'child');
+		await frame.waitForSelector('#get_parent_data');
+		const button = await frame.$('#get_parent_data');
+		await button.click();
+		await page.waitFor(500);
+		const logValue = await frame.evaluate(() => document.querySelector('#child_log').value);
+		expect(logValue).to.eql('This is data from parent');
+	});
+
+	it('Get parent error', async function() {
+		const frame = await page.frames().find(f => f.name() === 'child');
+		await frame.waitForSelector('#get_parent_error');
+		const button = await frame.$('#get_parent_error');
+		await button.click();
+		await page.waitFor(500);
+		const logValue = await frame.evaluate(() => document.querySelector('#child_log').value);
+		expect(logValue).to.have.string('Error of parent.getDataOfParent');
+	});
+
+	it('Get parent async data', async function() {
+		const frame = await page.frames().find(f => f.name() === 'child');
+		await frame.waitForSelector('#get_parent_async_data');
+		const button = await frame.$('#get_parent_async_data');
+		await button.click();
+		await page.waitFor(500);
+		const logValue = await frame.evaluate(() => document.querySelector('#child_log').value);
+		expect(logValue).to.eql('This is async data from parent');
+	});
+
+	it('Get parent async error', async function() {
+		const frame = await page.frames().find(f => f.name() === 'child');
+		await frame.waitForSelector('#get_parent_async_error');
+		const button = await frame.$('#get_parent_async_error');
+		await button.click();
+		await page.waitFor(500);
+		const logValue = await frame.evaluate(() => document.querySelector('#child_log').value);
+		expect(logValue).to.have.string('Error of parent.getAsyncErrorOfParent');
+	});
+
+	it('Get child data', async function() {
+		const button = await page.$('#get_child_data');
+		await button.click();
+		await page.waitFor(500);
+		const logValue = await page.evaluate(() => document.querySelector('#parent_log').value);
+		expect(logValue).to.eql('This is data from child');
+	});
+
+	it('Get child error', async function() {
+		const button = await page.$('#get_child_error');
+		await button.click();
+		await page.waitFor(500);
+		const logValue = await page.evaluate(() => document.querySelector('#parent_log').value);
+		expect(logValue).to.have.string('Error of child.getDataOfChild');
+	});
+
+	it('Get child async data', async function() {
+		const button = await page.$('#get_child_async_data');
+		await button.click();
+		await page.waitFor(500);
+		const logValue = await page.evaluate(() => document.querySelector('#parent_log').value);
+		expect(logValue).to.eql('This is async data from child');
+	});
+
+	it('Get child async error', async function() {
+		const button = await page.$('#get_child_async_error');
+		await button.click();
+		await page.waitFor(500);
+		const logValue = await page.evaluate(() => document.querySelector('#parent_log').value);
+		expect(logValue).to.have.string('Error of child.getAsyncErrorOfChild');
+	});
+});
